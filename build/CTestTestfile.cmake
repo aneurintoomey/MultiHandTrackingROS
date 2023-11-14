a5fceb7ad39510afa01747b0d80c0bfcb976fbd2
aneurin <aneurin@ROS1Noetic.myguest.virtualbox.org>
2023-11-14 16:01:40 -0700

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/aneurin/Documents/multi_hand_tracking_ws/src
+# Build directory: /home/aneurin/Documents/multi_hand_tracking_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
